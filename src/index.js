ec33e2e5995448b9143f4c94639569867a774f2d
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-07 16:45:32 +0400

    v1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import emojipedia from "./emojipedia.js";
 
-// var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5]
 
 //Map -Create a new array by doing something with each item in an array.
 
